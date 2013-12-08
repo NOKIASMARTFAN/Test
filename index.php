f93e6da2257d55599c063491f2cc26ec058f1631
NOKIASMARTFAN <Aleksandr_Zhuravel-Danilenko@ukr.net>
2013-12-08 18:55:22 +0200

    Translate

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Моя первая веб-страница</title>
+  <title>My first web-page</title>
  </head>
  <body>
 
