abf479f4ca3242733f48bfde37f59fcdc199c2d2
NOKIASMARTFAN <Aleksandr_Zhuravel-Danilenko@ukr.net>
2013-12-09 15:36:25 +0200

    3nd commit

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>My first web-page</title>
- </head>
- <body>
-
   <p>Основной текст.</p>
 
  </body>
